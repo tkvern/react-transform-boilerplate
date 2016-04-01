c0031d5749ac9b304807ff2fdc8f94fa4913dee8
BrandlVern <vern@BrandlVerns-MacBook-Pro.local>
2016-04-01 10:20:54 +0800

    add React

---
+++ src/shared/NavBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+class NavBar extends Component {
+  render() {
+    return(
+      <RaisedButton label="Default" />
+    );
+  }
+}
+
+export default NavBar;
